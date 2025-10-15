33963a13cf226a75d174b80d5afd18726c4fe77e
Kendree <94144671+kchenturtles@users.noreply.github.com>
2025-10-15 10:41:50 -0400

    fix: unstable component render

--- pages/MainTabs.tsx
+++ pages/MainTabs.tsx
@@ -8,6 +8,7 @@ import RecentlyPlayedPage from './RecentlyPlayedPage';
 import AboutPage from './AboutPage';
 
 const Tab = createBottomTabNavigator<any>();
+const renderTabBar = (props: any) => <BottomMenuBar {...props} />;
 
 function PlaceholderScreen({ title }: { title: string }) {
   return (
@@ -19,7 +20,7 @@ function PlaceholderScreen({ title }: { title: string }) {
 
 export default function MainTabs() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }} tabBar={(props) => <BottomMenuBar {...props} />}>
+    <Tab.Navigator screenOptions={{ headerShown: false }} tabBar={renderTabBar}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Recently Played" component={RecentlyPlayedPage} />
       <Tab.Screen name="Schedule" component={SchedulePage} />

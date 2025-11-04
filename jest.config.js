3560e9e25b6addbf0121b2f9d4cf6078b40111cf
Jay Sitter <jsit@users.noreply.github.com>
2025-11-03 22:55:10 -0500

    chore: use testing-library/react-native (#69)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native).*/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation).*/)',
   ],
   setupFilesAfterEnv: [
     './jest.setup.ts',

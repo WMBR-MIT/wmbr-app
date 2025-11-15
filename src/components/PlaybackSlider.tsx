06f72751623079fecb532b2a96a5db442a6aeec6
Jay Sitter <jsitter@mit.edu>
2025-11-15 12:55:04 -0500

    fix: make slider progress bar WMBR green

--- src/components/PlaybackSlider.tsx
+++ src/components/PlaybackSlider.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import TrackPlayer, { useProgress } from 'react-native-track-player';
 import { ViewStyle, StyleProp } from 'react-native';
+import { COLORS, CORE_COLORS } from '../utils/Colors';
 
 import Slider from '@react-native-community/slider';
 
@@ -30,6 +31,8 @@ export default function PlaybackSlider({
       minimumValue={0}
       maximumValue={1}
       onSlidingComplete={handleSlidingComplete}
+      minimumTrackTintColor={CORE_COLORS.WMBR_GREEN}
+      maximumTrackTintColor={COLORS.BACKGROUND.SECONDARY}
       onValueChange={handleValueChange}
       tapToSeek={true}
       value={progress.position / progress.duration || 0}

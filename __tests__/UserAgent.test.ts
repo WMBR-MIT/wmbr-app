ae281870f458ddf06658b70d80c0920c94998e98
Jay Sitter <jsitter@mit.edu>
2025-11-15 18:04:59 -0500

    style: lint

--- __tests__/UserAgent.test.ts
+++ __tests__/UserAgent.test.ts
@@ -17,27 +17,27 @@ describe('getUserAgent', () => {
   test('should return iOS user agent when platform is iOS', () => {
     (Platform as any).OS = 'ios';
     (Platform as any).Version = '17.0';
-    
+
     const userAgent = getUserAgent();
-    
+
     expect(userAgent).toBe('WMBRApp/0.0.1 (iOS; iPhone)');
   });
 
   test('should return Android user agent when platform is Android', () => {
     (Platform as any).OS = 'android';
     (Platform as any).Version = 33;
-    
+
     const userAgent = getUserAgent();
-    
+
     expect(userAgent).toBe('WMBRApp/0.0.1 (Android; SDK 33)');
   });
 
   test('should include correct SDK version for Android', () => {
     (Platform as any).OS = 'android';
     (Platform as any).Version = 30;
-    
+
     const userAgent = getUserAgent();
-    
+
     expect(userAgent).toContain('SDK 30');
   });
 

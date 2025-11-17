ee07dae195a88eecc64313e9e231f560ff928b5a
Jay Sitter <jsitter@mit.edu>
2025-11-17 14:20:46 -0500

    chore: reorder react native babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    'react-native-reanimated/plugin',
     [
       'module-resolver',
       {
@@ -16,5 +15,6 @@ module.exports = {
         },
       },
     ],
+    'react-native-reanimated/plugin',
   ],
 };

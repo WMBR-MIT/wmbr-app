d07297ab02a2c61d74005c3f66bbab57283b273a
Jay Sitter <jsit@users.noreply.github.com>
2025-11-19 18:56:25 -0500

    chore: add path aliases (#111)

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@services': './src/services',
+          '@utils': './src/utils',
+          '@customTypes': './src/types',
+          '@app': './src/app',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };

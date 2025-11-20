e9d825ceaa78340b1a190313ac5a0d406f8ca243
Jay Sitter <jsit@users.noreply.github.com>
2025-11-20 15:49:50 -0500

    test: add tests for Recently Played (#112)

--- jest.setup.ts
+++ jest.setup.ts
@@ -27,4 +27,4 @@ jest.mock('./src/utils/Debug.ts', () => ({
 
 // Mock fetch at the network boundary instead of mocking services
 // This allows real service code to run in tests
-global.fetch = createMockFetch() as any;
+jest.spyOn(global, 'fetch').mockImplementation(createMockFetch());

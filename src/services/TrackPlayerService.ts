e2cbcdd720639e96b9ba7d920f8cb75c5da458e5
alexandersimoes <alexandersimoes@gmail.com>
2025-11-21 17:09:00 -0500

    adds missing event listeners to handle lock screen jump forward/backward controls

--- src/services/TrackPlayerService.ts
+++ src/services/TrackPlayerService.ts
@@ -4,6 +4,18 @@ const TrackPlayerService = async () => {
   TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
   TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
   TrackPlayer.addEventListener(Event.RemoteStop, () => TrackPlayer.reset());
+
+  TrackPlayer.addEventListener(Event.RemoteJumpForward, async ({ interval }) => {
+    const { position, duration } = await TrackPlayer.getProgress();
+    const newPosition = Math.min(position + interval, duration);
+    await TrackPlayer.seekTo(newPosition);
+  });
+
+  TrackPlayer.addEventListener(Event.RemoteJumpBackward, async ({ interval }) => {
+    const { position } = await TrackPlayer.getProgress();
+    const newPosition = Math.max(position - interval, 0);
+    await TrackPlayer.seekTo(newPosition);
+  });
 };
 
 export default TrackPlayerService;

a9ed83de480d232e5d7f7ffe0d2080f5f07a0d57
Jay Sitter <jsitter@mit.edu>
2025-11-20 21:31:36 -0500

    chore: start normalizing colors

--- src/app/Schedule/SchedulePage.tsx
+++ src/app/Schedule/SchedulePage.tsx
@@ -392,7 +392,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   errorText: {
-    color: COLORS.TEXT.ERROR,
+    color: COLORS.TEXT.ALERT,
     textAlign: 'center',
     fontSize: 16,
     marginBottom: 20,
@@ -478,7 +478,7 @@ const styles = StyleSheet.create({
     color: COLORS.TEXT.PRIMARY,
   },
   showFrequency: {
-    color: COLORS.TEXT.META,
+    color: COLORS.TEXT.TERTIARY,
     fontSize: 11,
     fontStyle: 'italic',
     marginBottom: 4,

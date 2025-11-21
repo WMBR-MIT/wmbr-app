bab5ee239dce0c87ec3b68c7b01d0d852430f25c
Jay Sitter <jsit@users.noreply.github.com>
2025-11-21 18:08:48 -0500

    feat: change skip interval to 30

--- src/utils/TrackPlayerUtils.ts
+++ src/utils/TrackPlayerUtils.ts
@@ -1,6 +1,6 @@
 import { Capability } from 'react-native-track-player';
 
-export const SKIP_INTERVAL = 15;
+export const SKIP_INTERVAL = 30;
 
 export const archiveCapabilities = [
   Capability.Play,

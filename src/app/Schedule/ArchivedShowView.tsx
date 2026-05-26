143aec50861c623dfa1d4bf058a4e6f62db0add1
Jay Sitter <jsit@users.noreply.github.com>
2026-05-25 22:44:46 -0400

    chore: seekTo is async; await it, and add catch (#173)

--- src/app/Schedule/ArchivedShowView.tsx
+++ src/app/Schedule/ArchivedShowView.tsx
@@ -207,7 +207,12 @@ export default function ArchivedShowView() {
 
   const handleSkipBackward = async () => {
     const newPosition = Math.max(progress.position - SKIP_INTERVAL, 0);
-    await TrackPlayer.seekTo(newPosition);
+
+    try {
+      await TrackPlayer.seekTo(newPosition);
+    } catch (e) {
+      debugError('Error skipping backward:', e);
+    }
   };
 
   const handleSkipForward = async () => {
@@ -215,7 +220,12 @@ export default function ArchivedShowView() {
       progress.position + SKIP_INTERVAL,
       progress.duration,
     );
-    await TrackPlayer.seekTo(newPosition);
+
+    try {
+      await TrackPlayer.seekTo(newPosition);
+    } catch (e) {
+      debugError('Error skipping forward:', e);
+    }
   };
 
   return (

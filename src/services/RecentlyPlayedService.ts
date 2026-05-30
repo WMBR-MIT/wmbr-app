2e4c12dae69729a235ea48703312d947ecfc55b0
Jay Sitter <jsitter@mit.edu>
2026-05-30 19:06:30 -0400

    fix: return empty array on error

--- src/services/RecentlyPlayedService.ts
+++ src/services/RecentlyPlayedService.ts
@@ -86,7 +86,7 @@ export class RecentlyPlayedService {
       const data = await response.json();
 
       if (data.error) {
-        return data.error;
+        return [];
       }
 
       const playlist: PlaylistResponse = data as PlaylistResponse;

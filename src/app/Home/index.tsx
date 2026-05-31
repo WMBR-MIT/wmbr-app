99b6d436cef617e54080a179ae53d5df4efc155f
Jay Sitter <jsitter@mit.edu>
2026-05-31 11:18:52 -0400

    feat: add artist and song to live metadata

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -151,8 +151,10 @@ export default function HomeScreen() {
         // Only update if we're not playing an archive
         if (!archiveState.isPlayingArchive) {
           await TrackPlayer.updateMetadataForTrack(0, {
-            title: DEFAULT_NAME,
-            artist: currentShow || 'Live Radio',
+            title: currentShow || DEFAULT_NAME,
+            artist: showInfo
+              ? `${showInfo?.currentArtist} - ${showInfo.currentSong}`
+              : 'Live Radio',
             artwork: require('../../../assets/cover.png'),
           });
 
@@ -171,7 +173,8 @@ export default function HomeScreen() {
     currentShow,
     archiveState.isPlayingArchive,
     isPlayerInitialized,
-    playbackState?.state,
+    playbackState.state,
+    showInfo,
   ]);
 
   const togglePlayback = useCallback(async () => {

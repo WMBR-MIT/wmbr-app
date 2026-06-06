803c9111c871976407757ecf17e803ba84dd9927
Jay Sitter <jsitter@mit.edu>
2026-06-06 17:20:50 -0400

    fix: fix tests, iOS font bundle

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation).*/)',
+    // Transform react-native packages and vector-icons (which ships as TypeScript/ESM)
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-vector-icons).*/)',
   ],
+  moduleNameMapper: {
+    // Stub out binary font assets so Jest doesn't try to parse them
+    '\\.ttf$': '<rootDir>/__mocks__/fileMock.js',
+  },
   setupFilesAfterEnv: ['./jest.setup.ts'],
 };

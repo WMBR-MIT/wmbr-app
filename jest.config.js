9f43af54583725abea970f2786a9f8f4d66d1005
Jay Sitter <jsitter@mit.edu>
2026-06-06 17:57:51 -0400

    fix: remove extra jest config comment

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    // Transform react-native packages and vector-icons (which ships as TypeScript/ESM)
     'node_modules/(?!(react-native|@react-native|@react-navigation).*/)',
   ],
   setupFilesAfterEnv: ['./jest.setup.ts'],
